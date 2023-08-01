c8b249a4d3272e9c42b66ea72fa9f0f2ae8d21fb
Molosse34 <112559872+Molosse34@users.noreply.github.com>
2023-08-01 19:17:33 -0400

    Update vpn_selector.py
    it is the beta but it could work

--- vpn_selector.py
+++ vpn_selector.py
@@ -2,14 +2,50 @@
 
 import os
 import argparse
+import json 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--vpn", type=str, default="Default value", help="to choose the vpn")
-args = parser.parse_args()
+parser.add_argument( '-v',"--vpn", type=str, default="Default value", help="to choose the vpn")
+parser.add_argument("-c","--change", help="change the variable", action='store_true')
+args= parser.parse_args()
+
+
+if not os.path.exists('chemin_vpn_htb.json'):
+    with open('chemin_vpn_htb.json', 'w') as file:
+        json.dump({}, file)
+
+if not os.path.exists('chemin_vpn_thm.json'):
+    with open('chemin_vpn_thm.json', 'w') as file:
+        json.dump({}, file)
+
+
+with open('chemin_vpn_htb.json', 'r') as file:
+		a = json.load(file)
+with open('chemin_vpn_thm.json', 'r') as file:
+		b = json.load(file)
+
+
+if args.change == True:
+	a = input('écrit le chemain du vpn htb: ')
+	b = input('écrit le chemain du vpn thm: ')
+
+	with open('chemin_vpn_htb.json', 'w') as file:
+            json.dump(a, file)
+	with open('chemin_vpn_thm.json', 'w') as file:
+            json.dump(b, file)
+
+
 
 
 
 if args.vpn == "htb":
-	os.system('gnome-terminal -- bash -c "openvpn /usr/share/vpn/htb_vpn.ovpn"')
-if args.vpn == "thm":
-	os.system('gnome-terminal -- bash -c "openvpn /usr/share/vpn/thm_vpn.ovpn"')
+	try:
+		 os.system(f'gnome-terminal -- bash -c "openvpn {a}"')
+	finally:
+		print("wrong path")
+
+elif args.vpn == "thm":
+	try:
+		os.system(f'gnome-terminal -- bash -c "openvpn {b}"')
+	finally:
+		print('wrong path')
