672223028e7dbcf26361565b5c2cf3e2e9469248
Molosse34 <thomasba99@gmail.com>
2023-08-06 11:08:41 -0400

    auto-compleation on the input thanks to sof

--- vpn_selector.py
+++ vpn_selector.py
@@ -3,6 +3,29 @@
 import os
 import argparse
 import json 
+import pathlib
+import readline
+
+
+def complete_path(text, state):
+    incomplete_path = pathlib.Path(text)
+    if incomplete_path.is_dir():
+        completions = [p.as_posix() for p in incomplete_path.iterdir()]
+    elif incomplete_path.exists():
+        completions = [incomplete_path]
+    else:
+        exists_parts = pathlib.Path('.')
+        for part in incomplete_path.parts:
+            test_next_part = exists_parts / part
+            if test_next_part.exists():
+                exists_parts = test_next_part
+
+        completions = []
+        for p in exists_parts.iterdir():
+            p_str = p.as_posix()
+            if p_str.startswith(text):
+                completions.append(p_str)
+    return completions[state]
 
 parser = argparse.ArgumentParser()
 parser.add_argument( '-v',"--vpn", type=str, default="Default value", help="to choose the vpn")
@@ -10,28 +33,34 @@ parser.add_argument("-c","--change", help="change the variable", action='store_t
 args= parser.parse_args()
 
 
-if not os.path.exists('chemin_vpn_htb.json'):
-    with open('chemin_vpn_htb.json', 'w') as file:
+
+
+if not os.path.exists('/opt/vpn_selector/chemin_vpn_htb.json'):
+    with open('/opt/vpn_selector/chemin_vpn_htb.json', 'w') as file:
         json.dump({}, file)
 
-if not os.path.exists('chemin_vpn_thm.json'):
-    with open('chemin_vpn_thm.json', 'w') as file:
+if not os.path.exists('/opt/vpn_selector/chemin_vpn_thm.json'):
+    with open('/otp/vpn_selctor/chemin_vpn_thm.json', 'w') as file:
         json.dump({}, file)
 
 
-with open('chemin_vpn_htb.json', 'r') as file:
+with open('/opt/vpn_selector/chemin_vpn_htb.json', 'r') as file:
 		a = json.load(file)
-with open('chemin_vpn_thm.json', 'r') as file:
+with open('/opt/vpn_selector/chemin_vpn_thm.json', 'r') as file:
 		b = json.load(file)
 
 
 if args.change == True:
-	a = input('écrit le chemain du vpn htb: ')
-	b = input('écrit le chemain du vpn thm: ')
+	readline.set_completer_delims(' \t\n;')
+	readline.parse_and_bind("tab: complete")
+	readline.set_completer(complete_path)
+	
+	a = input('écrit le chemin du vpn htb: ')
+	b = input('écrit le chemin du vpn thm: ')
 
-	with open('chemin_vpn_htb.json', 'w') as file:
+	with open('/opt/vpn_selector/chemin_vpn_htb.json', 'w') as file:
             json.dump(a, file)
-	with open('chemin_vpn_thm.json', 'w') as file:
+	with open('/opt/vpn_selector/chemin_vpn_thm.json', 'w') as file:
             json.dump(b, file)
 
 
@@ -40,12 +69,12 @@ if args.change == True:
 
 if args.vpn == "htb":
 	try:
-		os.system(f'bash -c "openvpn {a}"')
+		os.system(f'gnome-terminal -- bash -c "openvpn {a}"')
 	except SyntaxError:
 		print("wrong path")
 
 elif args.vpn == "thm":
 	try:
-		os.system(f'bash -c "openvpn {b}"')
+		os.system(f'gnome-terminal -- bash -c "openvpn {b}"')
 	except SyntaxError:
 		print('wrong path')
